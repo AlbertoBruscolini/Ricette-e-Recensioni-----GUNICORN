56b4d37779de3b96b6aaf4967bfc826ff5e1c442
AlbertoBruscolini <alberto.bruscolini@studenti.isissgobetti.it>
2026-03-05 11:07:22 +0100

    Prepare for remote sync

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import create_app
+from dotenv import load_dotenv
+
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+app = create_app()
+if __name__ == '__main__':
+    app.run(debug=True)
